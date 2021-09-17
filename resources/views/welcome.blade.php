140508ece8583bb67fdb5244f002e4cf0b9a7ad7
farahmumtaz12@gmail.com <farahmumtaz12@gmail.com>
2021-09-17 10:37:18 +0700

    test lagi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TPQ BANYU URIP</title>
+</head>
+<body>
+<font face=”times new roman” size=”10″ color=”blue”>
+<center><h1>بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
+<h2>اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَ</h2>
+</body>
+</html>
