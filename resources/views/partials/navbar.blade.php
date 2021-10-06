68297662d5c2e670f64a16b9159ed97db93b762e
faraahsm <85088236+faraahsm@users.noreply.github.com>
2021-10-06 10:27:27 +0700

    update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bc8f8f;">
     <div class="container">
-      <a class="navbar-brand" href="/">TPQ Banyu Urip</a>
+      <a class="navbar-brand" href="{{ url('/')}}">TPQ Banyu Urip</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="/">Home</a>
+            <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="{{ url('/')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="/about">About</a>
+            <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="{{ url('/about')}}">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "Register") ? 'active' : '' }}" href="/register">Register</a>
+            <a class="nav-link {{ ($title === "Register") ? 'active' : '' }}" href="{{ url('/register')}}">Register</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "Login") ? 'active' : '' }}" href="/login">Login</a>
+            <a class="nav-link {{ ($title === "Login") ? 'active' : '' }}" href="{{ url('/login')}}">Login</a>
           </li>
         </ul>
       </div>
