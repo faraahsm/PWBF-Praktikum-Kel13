ece15cf926c052c9deaadf75f1554855b7a5e656
faraahsm <85088236+faraahsm@users.noreply.github.com>
2021-10-12 15:48:20 +0700

    add controller tabel

---
+++ app/Http/Controllers/controllerbuku.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\modelbuku;
+use Illuminate\Http\Request;
+
+class controllerbuku extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/controllerdetailkemajuan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\modeldetailkemajuan;
+use Illuminate\Http\Request;
+
+class controllerdetailkemajuan extends Controller
+{
+    //
+}
