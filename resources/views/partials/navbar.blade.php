70aca0f8eb2e1f1f6fc6d3dff15d1686f9175781
faraahsm <85088236+faraahsm@users.noreply.github.com>
2021-10-23 14:26:32 +0700

    update views add new navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,28 +1,40 @@
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container">
+<!-- Fixed navbar -->
+<div class="navbar navbar-inverse">
+  <div class="container">
+
+     
+      
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="{{ url('/')}}"><img src="img/logo.png" alt="logo"/></a>
+          <!-- Button for smallest screens -->
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+          <a class="navbar-brand" href="{{ url('/')}}">
+              <img src="public/assets/images/logo1.png" alt="TPQ Banyu Urip"></a>
       </div>
-      <div class="navbar-collapse collapse ">
-        <ul class="nav navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="{{ url('/')}}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="{{ url('/about')}}">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "Register") ? 'active' : '' }}" href="{{ url('/register')}}">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "Login") ? 'active' : '' }}" href="{{ url('/login')}}">Login</a>
-          </li>
-        </ul>
+
+      <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav pull-right">
+              <li class="nav-item">
+                  <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="{{ url('/')}}">Beranda</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="{{ url('/about')}}">Tentang</a>
+              </li>
+              <li class="dropdown">
+                  <a href="{{ url('/')}}" class="dropdown-toggle" data-toggle="dropdown">Pengguna<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                      <li><a class="nav-link {{ ($title === "Santri") ? 'active' : '' }}" href="{{ url('/pengguna/santri')}}">Santri</a></li>
+                      <li><a class="nav-link {{ ($title === "Pengurus") ? 'active' : '' }}" href="{{ url('/pengguna/pengurus')}}">Pengurus</a></li>
+                      <li><a class="nav-link {{ ($title === "Buku") ? 'active' : '' }}" href="{{ url('/pengguna/buku')}}">Buku</a></li>
+                      <li><a class="nav-link {{ ($title === "Bab") ? 'active' : '' }}" href="{{ url('/pengguna/bab')}}">Bab</a></li>
+                  </ul>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link {{ ($title === "Akun") ? 'active' : '' }}" href="{{ url('/akun')}}">Akun</a>
+              </li>
+
+          </ul>
       </div>
-    </div>
-</nav>
+      <!--/.nav-collapse -->
+  </div>
+</div>
+<!-- /.navbar -->
